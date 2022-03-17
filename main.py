fcc9d29923d3bd7e56a8603eae1f5b329c2ad1e1
Tyler Russin <tylerrussin2@gmail.com>
2022-03-16 17:54:36 -0700

    Update color

--- main.py
+++ main.py
@@ -26,10 +26,17 @@ def flood_fill(screen, sr, sc, new_color, old_color, view):
 
         # Display current screen
         screen[nScreenHeight - 1][nScreenWidth - 1] = ''
-        view.addstr(0, 0, ''.join(str(ele) for sub in screen for ele in sub))
+        view.addstr(0, 0, ''.join(str(ele) for sub in screen for ele in sub), curses.color_pair(1))
+
+        for p1, row in enumerate(screen):
+            for p2, value in enumerate(row):
+                if value == 2:
+                    view.addstr(p1, p2, ' ', curses.color_pair(2))
+
+        view.addstr(nScreenHeight - 1, nScreenWidth - 1, '')
         view.refresh()
 
-        time.sleep(.1)   # Slow down traversal
+        time.sleep(.0001)   # Slow down traversal
 
         # North
         if sr >= 1: 
@@ -53,14 +60,17 @@ if __name__ == '__main__':
     command_line_font(16)       # Console ASCII font size
     nScreenWidth = 120          # Console Screen Size X (columns)
     nScreenHeight = 40          # Console Screen Size Y (rows)
-    sr = 11                      # Color change starting coords
-    sc = 45
+    sr = 20                      # Color change starting coords
+    sc = 60
     new_color = 2               # Color to be changed to
 
     # Create Screen Buffer
-    screen = [[random.choice([0,0,0,1]) for x in range(nScreenWidth)] for y in range(nScreenHeight)]
+    screen = [[random.choice([' ',' ', u'\u2588']) for x in range(nScreenWidth)] for y in range(nScreenHeight)]
     os.system(f"mode con: cols={nScreenWidth} lines={nScreenHeight}")
     view = curses.initscr()
+    curses.start_color()                                            # Enables color
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)     # Setting color pairs
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
     # Set old color
     old_color = screen[sr][sc]
@@ -68,3 +78,24 @@ if __name__ == '__main__':
     # Preform transformation
     if new_color != old_color:
         flood_fill(screen, sr, sc, new_color, old_color, view)
+
+        while 1:
+
+            # Quit simulation
+            if keyboard.is_pressed('Q'):
+                curses.endwin()                                 # End curses instance
+                os.system(f"mode con: cols={120} lines={40}")   # Reset screen buffer
+                command_line_font(16)                           # Reset default font size
+                sys.exit()  
+
+            # Display current screen
+            screen[nScreenHeight - 1][nScreenWidth - 1] = ''
+            view.addstr(0, 0, ''.join(str(ele) for sub in screen for ele in sub), curses.color_pair(1))
+
+            for p1, row in enumerate(screen):
+                for p2, value in enumerate(row):
+                    if value == 2:
+                        view.addstr(p1, p2, ' ', curses.color_pair(2))
+
+            view.addstr(nScreenHeight - 1, nScreenWidth - 1, '')
+            view.refresh()
