007abf22f4b606e6dc4bae4544f0d2237e872d33
Tyler Russin <tylerrussin2@gmail.com>
2022-03-16 17:09:01 -0700

    Base implementation

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import os
 import sys
+import time
+import random
 import keyboard
 import curses
 
 from Command_Line_Font import command_line_font
 
 
-command_line_font(16)        # Console ASCII font size
-nScreenWidth = 120          # Console Screen Size X (columns)
-nScreenHeight = 40          # Console Screen Size Y (rows)
-
-# Create Screen Buffer
-screen = [[' ' for x in range(nScreenWidth)] for y in range(nScreenHeight)]
-os.system(f"mode con: cols={nScreenWidth} lines={nScreenHeight}")
-view = curses.initscr()
-
-while 1:
+def flood_fill(screen, sr, sc, new_color, old_color, view):
+    '''Conversts values in a matrix in a flood fill
+       pattern using depth first search'''
 
     # Quit simulation
     if keyboard.is_pressed('Q'):
@@ -24,13 +19,52 @@ while 1:
         command_line_font(16)                           # Reset default font size
         sys.exit()                                      # Exit Program
 
+    # Transfrom value
+    if screen[sr][sc] == old_color:
+
+        screen[sr][sc] = new_color  # Set to new color
+
+        # Display current screen
+        screen[nScreenHeight - 1][nScreenWidth - 1] = ''
+        view.addstr(0, 0, ''.join(str(ele) for sub in screen for ele in sub))
+        view.refresh()
+
+        time.sleep(.1)   # Slow down traversal
+
+        # North
+        if sr >= 1: 
+            flood_fill(screen, sr-1, sc, new_color, old_color, view)
+
+        # South
+        if sr+1 < len(screen): 
+            flood_fill(screen, sr+1, sc, new_color, old_color, view)
+
+        # West
+        if sc >= 1: 
+            flood_fill(screen, sr, sc-1, new_color, old_color, view)
+
+        # East
+        if sc+1 < len(screen[0]): 
+            flood_fill(screen, sr, sc+1, new_color, old_color, view)
 
 
-    screen[20][40] = 'T'
+if __name__ == '__main__':
 
-    # Display Frame (problem with the \0 in python 3.9. empty string also works. null termination only needed for c++ version)
-    screen[nScreenHeight - 1][nScreenWidth - 1] = ''
-    view.addstr(0, 0, ''.join(ele for sub in screen for ele in sub))
-    view.refresh()
+    command_line_font(16)       # Console ASCII font size
+    nScreenWidth = 120          # Console Screen Size X (columns)
+    nScreenHeight = 40          # Console Screen Size Y (rows)
+    sr = 11                      # Color change starting coords
+    sc = 45
+    new_color = 2               # Color to be changed to
 
+    # Create Screen Buffer
+    screen = [[random.choice([0,0,0,1]) for x in range(nScreenWidth)] for y in range(nScreenHeight)]
+    os.system(f"mode con: cols={nScreenWidth} lines={nScreenHeight}")
+    view = curses.initscr()
 
+    # Set old color
+    old_color = screen[sr][sc]
+
+    # Preform transformation
+    if new_color != old_color:
+        flood_fill(screen, sr, sc, new_color, old_color, view)
